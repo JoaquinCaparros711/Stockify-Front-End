296552d237dc30bd9bbf9307c2b5987072eea14d
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2025-08-20 13:53:49 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // La URL base de tu API de Django.
-const baseURL = 'http://127.0.0.1:8000/api';
+const baseURL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
+
 
 const api = axios.create({
     baseURL,
