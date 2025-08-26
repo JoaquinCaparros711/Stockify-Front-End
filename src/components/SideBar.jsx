3d237ed8c098725879b16bcbc6689aea7de0f24b
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2025-08-26 20:42:13 -0300

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,6 +8,7 @@ import { useData } from "../context/DataContext"
 import { Dropdown, Modal, Button, Form, Row, Col } from "react-bootstrap"
 import { BsList, BsExclamationTriangleFill, BsPencilFill, BsXCircleFill, BsCheckCircleFill } from "react-icons/bs"
 import "./SideBar.css"
+import logo from '../assets/Logo.png'
 
 // --- Componentes de Notificación ---
 const AppleStyleAlert = ({ message, onClose }) => {
@@ -145,6 +146,7 @@ const SideBar = () => {
       <div className={`sidebar-apple ${isOpen ? "open" : ""}`}>
         <div className="sidebar-header-apple">
           <div className="sidebar-logo-apple">Stockify</div>
+          <img src={logo} alt="" />
         </div>
         <nav className="sidebar-nav-apple">
           <ul>
